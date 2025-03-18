6295b27398e6aab2f46d8f21a9b481cc724a10c0
devken0 <119792185+devken0@users.noreply.github.com>
2025-03-19 00:12:05 +0800

    Update main.js
    Changelog:
    
    - Define getRandomInt function in pseudocode and actual code
    - Call getRandomInt function in pseudocode and actual code
    - Add console.log lines for testing purposes
    - Add descriptive comments for future reference

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
 /* 
 The program: Build a console-based rock paper scissors game
 
+DEFINE getRandomInt
+  INIT minCeiled 
+  INIT maxFloored
+  INIT randomInt
+  RETURN randomInt
 DEFINE getComputerChoice
   INIT randomInt to random number from 0-3
   INIT computerChoice
@@ -12,8 +17,25 @@ DEFINE getComputerChoice
     SET computerChoice to "scissors" 
   ENDIF
   RETURN computerChoice
+
+CALL getRandomInt with min and max as parameters
+CALL getComputerChoice
 */
 
+// Getting a random integer between two values.
+// It returns a random integer between the specified values. 
+// The value is no lower than min (or the next integer greater than min if min isn't an integer), 
+// and is less than or equal to max.
+function getRandomInt(min, max){
+  let minCeiled = Math.ceil(min);
+  let maxFloored = Math.floor(max);
+  let randomInt = Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled); // The maximum is inclusive and the minimum is inclusive
+  return randomInt;
+}
+
 let userName = "Ken";
-let greetings = "Hello " + userName;
+let greetings = "Horray " + userName + ", your code runs!";
 console.log(greetings);
+
+console.log(getRandomInt(1, 3));
+
