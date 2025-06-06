6a23b32358d1767f4de30ca83b86177809a34fc4
devken0 <119792185+devken0@users.noreply.github.com>
2025-06-06 12:58:07 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,112 +1,132 @@
-/* 
-The program: Build a console-based rock paper scissors game
-*/
+const intro = document.querySelector("#intro");
+const rounds = document.querySelector("#rounds");
+const scores= document.querySelector("#scores");
+const gameWinner = document.querySelector("#game-winner");
+const buttons = document.querySelector("#buttons");
+const resetBtn = document.createElement("button");
+const quit = document.createElement("p");
+const playerScoreDisplay = document.createElement("span");
+const computerScoreDisplay = document.createElement("span");
+
+let playerScore = 0;
+let computerScore = 0;
+let round = 1;
 
 // Getting a random integer between two values.
 // It returns a random integer between the specified values. 
 // The value is no lower than min (or the next integer greater than min if min isn't an integer), 
 // and is less than or equal to max.
 function getRandomInt(min, max){
-  let minCeiled = Math.ceil(min);
-  let maxFloored = Math.floor(max);
-  let randomInt = Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled); // The maximum is inclusive and the minimum is inclusive
-  return randomInt;
+  return Math.floor(Math.random() * (Math.floor(max) - Math.ceil(min) + 1) + Math.ceil(min)); // The maximum is inclusive and the minimum is inclusive
 }
 
-function getComputerChoice(){
-  const randomInt = getRandomInt(0, 2);
-  let computerChoice;
-
-  switch (randomInt) {
-    case 0:
-      computerChoice = "rock";
-      break;
-    case 1:
-      computerChoice = "paper";
-      break;
-    case 2:
-      computerChoice = "scissors";
-      break;
-  }
-  return computerChoice;
+function getComputerSelection(){
+  return ["rock", "paper", "scissors"][getRandomInt(0,2)];
 }
 
-function getHumanChoice(){
-  let humanChoice = String(prompt("Enter your choice: "));
-  return humanChoice;
+function getRoundWinner(playerSelection, computerChoice){
+  if (playerSelection === computerChoice) return "Draw";
+  if (
+    (playerSelection === "rock" && computerChoice === "scissors") || 
+    (playerSelection === "paper" && computerChoice === "rock") ||
+    (playerSelection === "scissors" && computerChoice === "paper")
+  ){
+    return "Human";
+  } 
+  return "Computer"
 }
 
-function getRoundWinner(humanChoice, computerChoice){
-  let roundWinner = "";
-  switch(humanChoice){
-    case "rock":
-      if(computerChoice == "paper"){
-        console.log("You lose! Paper beats Rock");
-        roundWinner = "Computer";
-      } else if(computerChoice == "scissors"){
-        console.log("You win! Rock beats Scissors");
-        roundWinner = "Human";
-      } else {
-        roundWinner = "Draw";
-      }
-      break;
-    case "paper":
-      if(computerChoice == "rock"){
-        console.log("You win! Paper beats Rock");
-        roundWinner = "Human";
-      } else if(computerChoice == "scissors"){
-        console.log("You lose! Scissors beats Paper");
-        roundWinner = "Computer";
-      } else {
-        roundWinner = "Draw";
-      }
-      break;
-    case "scissors":
-      if(computerChoice == "rock"){
-        console.log("You lose! Rock beats Scissors");
-        roundWinner = "Computer";
-      } else if(computerChoice == "paper"){
-        console.log("You win! Scissors beats Paper");
-        roundWinner = "Human";
-      } else {
-        roundWinner = "Draw";
-      }
-      break;
-  }
-  return roundWinner;
-}
-
-function playGame(){
-  function playRound(humanChoice, computerChoice){
-    humanChoice = humanChoice.toLowerCase();
-    const roundWinner = getRoundWinner(humanChoice, computerChoice);
-    if (roundWinner == "Human"){
-      humanScore == ++humanScore;
-    } else if (roundWinner == "Computer")  {
-      computerScore == ++computerScore;
-    } else if (roundWinner == "Draw"){
-      console.log("Draw!");
-    } else {
-      console.log("Oops!");
-    }
-  }
-
-  let humanScore = 0;
-  let computerScore = 0;
-
-  // for(let i = 1; i < 6; ++i){
-  //   const humanSelection = getHumanChoice();
-  //   const computerSelection = getComputerChoice();
-  //   playRound(humanSelection, computerSelection);
-  //   console.log("You: " + humanScore + "\tComputer: " + computerScore + "\t\tRound: " + i + "/5");
-  // }
-  if(humanScore == computerScore){
-    alert("Game is a draw! You know me too well");
-  } else if(humanScore > computerScore){
-    alert("Congrats! You just won the game");
+function addScore(winner){
+  if (winner == "Human"){
+    playerScore++;
+    playerScoreDisplay.style.color = "orange";
+    console.log("Player wins the round!");
+  } else if (winner == "Computer")  {
+    computerScore++;
+    computerScoreDisplay.style.color = "orange";
+    console.log("Computer wins the round!");
+  } else if (winner == "Draw"){
+    playerScoreDisplay.style.color = "red";
+    computerScoreDisplay.style.color = "red";
+    console.log("Its a draw!");
   } else {
-    alert("Game over. Better luck next time!");
+    console.log("Oops!");
+  }
+  return;
+}
+
+function resetGame(){
+  playerScore = 0; 
+  computerScore = 0;
+  round = 1;
+  console.log("Resetting...");
+  rounds.textContent = "Game has been reset, choose your pick!";
+  scores.innerHTML = '';
+  gameWinner.innerHTML = '';
+  const element = document.querySelector("#resetBtn");
+  if (element){
+    element.remove();
+  }
+  if (!buttons){
+    const div = document.createElement("div");
+    const btn = document.createElement("b");
+    const quit = document.body.lastChild;
+
+    div.setAttribute("id", "buttons");
+    document.body.insertBefore(div, quit);
+
+    btn.setAttribute("id", "rock");
+    buttons.appendChild(btn);
+    btn.setAttribute("id", "paper");
+    buttons.appendChild(btn);
+    btn.setAttribute("id", "scissors");
+    buttons.appendChild(btn);
   }
 }
 
-playGame();
+function playRound(playerSelection){
+  const computerChoice = getComputerSelection();
+  const winner = getRoundWinner(playerSelection, computerChoice);
+  console.log(`You chose ${playerSelection}, Computer chose ${computerChoice}`);
+
+  playerScoreDisplay.style.color = "black";
+  computerScoreDisplay.style.color = "black";
+  
+  addScore(winner);
+  intro.remove();
+  resetBtn.innerHTML = `<i class="fa fa-refresh" aria-hidden="true"></i>`;
+  resetBtn.setAttribute("id", "resetBtn");
+  buttons.appendChild(resetBtn);
+  resetBtn.addEventListener("click", () => resetGame());
+
+  rounds.textContent = `Player chose ${playerSelection}, Computer chose ${computerChoice}`;  
+  scores.textContent = "Player: ";
+  playerScoreDisplay.textContent = `${playerScore}`;
+  scores.appendChild(playerScoreDisplay);
+  const spacer = document.createTextNode("  Computer: ");
+  scores.appendChild(spacer);
+  computerScoreDisplay.textContent = `${computerScore}`;
+  scores.appendChild(computerScoreDisplay);
+
+  round++;
+
+  console.log("You: " + playerScore + "\tComputer: " + computerScore);
+
+  if (playerScore === 5 || computerScore === 5){
+    console.log(playerScore > computerScore ? "You win the game!" : "Computer wins!");
+    buttons.remove();
+    playerScore > computerScore ? gameWinner.textContent = "You win!" : gameWinner.textContent = "You lose!";
+    playerScore > computerScore ? gameWinner.style.color = "green" : gameWinner.style.color = "red";
+    //document.body.appendChild(resetBtn);
+  } 
+
+  quit.innerHTML = '<a href="#">Quit</a>';
+  document.body.appendChild(quit);
+
+  quit.addEventListener("click", () => location.reload(true));
+}
+
+document.querySelector("#rock").addEventListener("click", () => playRound("rock"));
+document.querySelector("#paper").addEventListener("click", () => playRound("paper"));
+document.querySelector("#scissors").addEventListener("click", () => playRound("scissors"));
+
